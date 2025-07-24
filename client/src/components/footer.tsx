99c39036d2624794639e74ae5ebb0ba66e36e04a
Amit Singh <4313535+amitsingh9thjan@users.noreply.github.com>
2025-07-24 20:43:42 +0530

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,18 +1,3 @@
 export default function Footer() {
-  const scrollToSection = (sectionId: string) => {
-    const element = document.getElementById(sectionId);
-    if (element) {
-      const offsetTop = element.offsetTop - 80;
-      window.scrollTo({
-        top: offsetTop,
-        behavior: "smooth",
-      });
-    }
-  };
-
- 
-
-  return (
-    
-  );
+  
 }
